5100536acde5bf66310c2f6b7a2f95e58cabefde
Raiden <diablo_doomzy@yahoo.com>
2017-11-11 16:35:15 -0800

    L218

--- Source/S05_TestingGrounds/ChooseNextWayPointTask.cpp
+++ Source/S05_TestingGrounds/ChooseNextWayPointTask.cpp
@@ -1,10 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWayPointTask.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 
 
 EBTNodeResult::Type UChooseNextWayPointTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - YAY!"));
+	//UE_LOG(LogTemp, Warning, TEXT("AI in C++ - YAY!"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
 
--- Source/S05_TestingGrounds/ChooseNextWayPointTask.h
+++ Source/S05_TestingGrounds/ChooseNextWayPointTask.h
@@ -16,5 +16,10 @@ class S05_TESTINGGROUNDS_API UChooseNextWayPointTask : public UBTTaskNode
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
-	
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+
 };
